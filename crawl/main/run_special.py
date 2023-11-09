2e08c375e659177c0c2024dc4f759f106d7edd71
Lawrence-AIP <liuch@allinfinance.com>
2023-11-09 17:11:05 +0800

    push

--- crawl/main/run_special.py
+++ crawl/main/run_special.py
@@ -4,9 +4,9 @@ from crawl.spiderDealer.sourceDeal import run_specific
 
 try:
     # 上传倒数第几页数的视频
-    page_num = 1
+    page_num = 2
     # 此次视频上传数量
-    nums = 2
+    nums = 3
     run(run_specific(page_num, nums))
 except Exception as e:
     print("main error:", e)
