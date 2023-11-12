57637a171993d9b2f2a3a7d7eae76eace74d37f1
Lawrence <aceliuchanghong@gmail.com>
2023-11-12 14:02:33 +0800

    push

--- crawl/main/run_special.py
+++ crawl/main/run_special.py
@@ -4,7 +4,7 @@ from crawl.spiderDealer.sourceDeal import run_specific
 
 try:
     # 上传倒数第几页数的视频
-    page_num = 2
+    page_num = 3
     # 此次视频上传数量
     nums = 2
     run(run_specific(page_num, nums))
