8f452c7f85fcf905d68882b911ea82d58efe39e9
Lawrence-AIP <liuch@allinfinance.com>
2023-11-28 17:33:41 +0800

    push

--- crawl/main/run_special.py
+++ crawl/main/run_special.py
@@ -4,7 +4,7 @@ from crawl.spiderDealer.sourceDeal import run_specific
 
 try:
     # 上传倒数第几页数的视频
-    page_num = 4
+    page_num = 5
     # 此次视频上传数量
     nums = 2
     run(run_specific(page_num, nums))
