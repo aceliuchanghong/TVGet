92d3e923f7070fe02db9e4a995495ff7b64f6c4a
lawrence <aceliuchanghong@gmail.com>
2023-12-05 08:14:50 +0800

    push

--- crawl/main/run_ytub.py
+++ crawl/main/run_ytub.py
@@ -5,7 +5,7 @@ output_path = "../../crawl/files/youtube/hitomi/out"
 
 try:
     # 此次视频上传数量
-    nums = 1
+    nums = 5
     run_youtube(output_path, nums)
 except Exception as e:
     print("main error:", e)
